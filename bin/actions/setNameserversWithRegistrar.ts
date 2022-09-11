ca07280c32432a62d23acdfc887c65858db63c0a
Konrad Markus <konker@iki.fi>
2022-09-11 14:59:26 +0300

    Add confirmation prompt for set nameservers command

--- bin/actions/setNameserversWithRegistrar.ts
+++ bin/actions/setNameserversWithRegistrar.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import type Vorpal from 'vorpal';
 
 import * as secretsmanager from '../../lib/aws/secretsmanager';
@@ -12,13 +13,6 @@ export function actionSetNameServersWithRegistrar(vorpal: Vorpal, config: SomCon
       vorpal.log('ERROR: no registrar specified in manifest');
       return;
     }
-    state.spinner.start();
-    const registrarConnector = getRegistrarConnector(state.registrar);
-    const somSecrets = await secretsmanager.getSomSecrets(config, DEFAULT_AWS_REGION, registrarConnector.SECRETS);
-    if (!registrarConnector.SECRETS.every((secretName) => somSecrets[secretName])) {
-      vorpal.log(`ERROR: secrets required by registrar connector missing: ${registrarConnector.SECRETS}`);
-      return;
-    }
 
     const nameservers = getParam(state, 'hosted-zone-name-servers')?.split(',');
     if (!nameservers || nameservers.length === 0) {
@@ -26,9 +20,30 @@ export function actionSetNameServersWithRegistrar(vorpal: Vorpal, config: SomCon
       return;
     }
 
-    const result = await registrarConnector.setNameServers(somSecrets, state.rootDomain as string, nameservers);
-    state.spinner.stop();
+    const response = await vorpal.activeCommand.prompt({
+      type: 'input',
+      name: 'confirm',
+      message: chalk.green(
+        `Are you sure you want to set registrar nameservers to: ${chalk.bold(nameservers.join(','))}? [y/n] `
+      ),
+    });
+    if (response.confirm === 'y') {
+      state.spinner.start();
+      try {
+        const registrarConnector = getRegistrarConnector(state.registrar);
+        const somSecrets = await secretsmanager.getSomSecrets(config, DEFAULT_AWS_REGION, registrarConnector.SECRETS);
+        if (!registrarConnector.SECRETS.every((secretName) => somSecrets[secretName])) {
+          vorpal.log(`ERROR: secrets required by registrar connector missing: ${registrarConnector.SECRETS}`);
+          return;
+        }
 
-    vorpal.log(`Set nameservers: ${result}`);
+        const result = await registrarConnector.setNameServers(somSecrets, state.rootDomain as string, nameservers);
+        vorpal.log(`Set nameservers: ${result}`);
+      } catch (ex: any) {
+        vorpal.log(`Error setting nameservers: ${ex}`);
+      } finally {
+        state.spinner.stop();
+      }
+    }
   };
 }
